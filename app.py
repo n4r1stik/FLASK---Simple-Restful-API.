5b17d0c171f8de0c3fc5d3cfef64980875f7aa2a
n4r1stik <levyanthy64@gmail.com>
2023-08-19 00:42:54 +0700

    menambahkan resource

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+api = Api(app)
+
+CORS(app)
+
+identitas = {}
+
+class ControlResource(Resource):
+
+    def get(self):
+        return identitas
+
+    def post(self):
+        nama = request.form['nama']
+        umur = request.form['umur']
+        identitas['nama'] = nama
+        identitas['umur'] = umur
+        response = f'Data berhasil dimasukan'
+        return response
+
+
+api.add_resource(ControlResource,"/api", methods=['GET','POST'])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
